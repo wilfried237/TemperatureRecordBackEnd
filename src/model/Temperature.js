ed18e88a05adca15ad31a1f14d06a552f8027493
wilfried kamdoum <kamdoumwilfried8@gmail.com>
2023-12-04 16:28:25 +0100

    modified params

--- src/model/Temperature.js
+++ src/model/Temperature.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 const TemperatureSchema = new mongoose.Schema({
     celcius: {type: Number, required: true},
     fahrenheit: {type: Number, float:true, required: true},
-    time: {type:Date, required:true},
+    time: {type:Date,  default: Date.now},
     userOwner: {type: mongoose.Schema.Types.ObjectId, ref:"Users", required: true}
 });
 
--- src/routes/temperature.js
+++ src/routes/temperature.js
@@ -16,14 +16,15 @@ router.get("/getTemperature/:userEmail", verificationToken, async(req,res)=>{
     }
 });
 
-router.post("/createTemperature/:userEmail", async(req, res)=>{
+router.post("/createTemperature/:userEmail/:celcius/:fahrenheit", async(req, res)=>{
     try{
-        const {celcius, fahrenheit,time} = req.body;
+        const celcius = req.params.celcius;
+        const fahrenheit = req.params.fahrenheit;
         const user = await UserModel.findOne({email:req.params.userEmail});
         if(!user){
             return res.statusCode(404).json({message: "The user doesn't exist"});
         }
-        const newTemp = new TemperatureModel({celcius,fahrenheit,time,userOwner:user._id});
+        const newTemp = new TemperatureModel({celcius:celcius,fahrenheit:fahrenheit,userOwner:user._id});
         await newTemp.save();
         res.status(200).json({message: "successfully Created record"});
     }
