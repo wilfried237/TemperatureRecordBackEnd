1fc5f0a9a421aba2bcf29e83a1c38dca2e66d3b2
wilfried kamdoum <kamdoumwilfried8@gmail.com>
2025-05-08 00:32:53 +0100

    fixed error

--- src/routes/temperature.js
+++ src/routes/temperature.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { SensorDataModel } from '../models/SensorData.js';
+import { SensorDataModel } from '../model/SensorDataModel';
 
 const router = express.Router();
 
