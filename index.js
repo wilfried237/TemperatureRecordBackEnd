bdc0eccfd117212f48b2a8637244b441c6bd92ce
wilfried kamdoum <kamdoumwilfried8@gmail.com>
2025-05-08 02:01:39 +0100

    fix error

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import {temperatureRouter} from './src/routes/temperature.js';
+import cors from 'cors';
+import { temperatureRouter } from './src/routes/temperature.js';
 
 dotenv.config();
 
 const app = express();
 
-app.use(express.json());  // for parsing application/json
+app.use(cors()); // Allow all origins
+app.use(express.json());
 
 app.use('/Temperature', temperatureRouter);
 
-mongoose.connect(process.env.REACT_APP_MONGOOSE_URL).then(
-    ()=>{
-    app.listen(process.env.PORT || 3001, ()=> console.log('server started')); 
-
-    }
-).catch((error)=>{
+mongoose.connect(process.env.REACT_APP_MONGOOSE_URL)
+  .then(() => {
+    app.listen(process.env.PORT || 3001, () => console.log('server started'));
+  })
+  .catch((error) => {
     console.error(error);
-});  
+  });
